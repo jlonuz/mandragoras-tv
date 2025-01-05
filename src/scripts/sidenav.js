5af407ca4380d45ecdd734d3ea1791986f2be973
Josefina Lonuz <jlonuz@Ordenador-portatil-de-Josefina.local>
2025-01-05 14:36:36 -0300

    avances sidenav, contacto y ajustes en mobile

---
+++ src/scripts/sidenav.js
@@ -0,0 +1,19 @@
+document.addEventListener('astro:page-load', function() {
+    const menuButton = document.querySelector('#menu-button');
+    const nav = document.querySelector('#mySidenav');
+    const main = document.querySelector('#main');
+    const body = document.querySelector('body');
+    menuButton?.addEventListener('click', () => {
+        console.log('click');
+        nav.style.width = '100%';
+        main.style.marginRights = '250px';
+        body.style.backgroundColor = 'rgba(0,0,0,0.4)';
+    });
+
+    document.querySelector('.closebtn').addEventListener('click', () => {
+        console.log('cierra menu');
+        nav.style.width = '0';
+        main.style.margin = '0';
+        body.style.backgroundColor = 'black';
+    });
+});
